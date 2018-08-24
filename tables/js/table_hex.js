90f0050e72631290a5562f17352e825c606a92c1
kkkkkst <mennlmkso2sr@gmail.com>
2018-08-25 04:16:19 +0900

    hexajoy ui

---
+++ tables/js/table_hex.js
@@ -0,0 +1,168 @@
+var SABUN_DL_STR = "DL";
+
+$(document).ready( function () {
+
+    $('#difficulty_table').DataTable({
+        lengthChange: false,
+        info: false,
+        paging: false,
+
+        "language": {
+            url:"//cdn.datatables.net/plug-ins/1.10.19/i18n/Japanese.json"
+        },
+
+        "ajax": {
+            url: "https://script.google.com/macros/s/AKfycbxMG5jqTHVZqSqE3dNkvc-QUcgm1-soiy0xMOJQn0ycCy2ZBGiy/exec?tablename=hexajoy",
+            dataSrc: ""
+        },
+
+        columns: [
+            {
+                title: 'level',
+                data: 'level',
+            },
+            {
+                title: 'タイトル<br>(LR2IR)',
+                data: 'title',
+                width: "25%",
+                render: ParseData.parseTitle,
+            },
+            {
+                title: 'アーティスト<br>(本体URL)',
+                data: 'artist',
+                width: "15%",
+                render: ParseData.parseArtist,
+            },
+            {
+                title: '差分',
+                render: ParseData.parseSabun,
+                orderable: false,
+            },
+            {
+                title: '日付',
+                data: 'date',
+                render: ParseData.parseDate,
+            },
+            {
+                title: 'BPM',
+                data: 'main_bpm',
+            },
+            {
+                title: 'Total/Notes<br>(回復量)',
+                render: ParseData.parseTotalnotes,
+            },
+            {
+                title: '譜面傾向',
+                width: "8%",
+                data: 'score_type',
+                render: ParseData.parseType,
+            },
+            {
+                title: 'コメント',
+                data: 'comment',
+            }
+        ],
+
+        createdRow: function(row, data){
+            if ( data.state == 1 ) {
+                $(row).addClass( 'new' );
+            }
+            if ( data.state == 2 ) {
+                $(row).addClass( 'update' );
+            }
+            if ( data.state == 5 ) {
+                $(row).addClass( 'deprecated' );
+            }
+        },
+
+    });
+
+
+
+
+
+
+
+} );
+
+var ParseData = {
+
+    parseTitle: function ( data, type, row) {
+        var link = 'http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=';
+        link += row.md5;
+        return '<a href="' + link + '" target="_blank">' + data + '</a>';
+    },
+
+    parseArtist: function(data,type,row){
+        var astr = "";
+
+        if(row.url) {
+            if(row.artist) {
+                astr = "<a href='" + row.url + "'>" + row.artist + "</a>";
+            } else {
+                astr = "<a href='" + row.url + "'>" + row.url + "</a>";
+            }
+        } else {
+            if(row.artist) {
+                astr = row.artist;
+            }
+        }
+
+        if(row.url_pack) {
+            if(row.name_pack) {
+                astr += "<br />(<a href='" + row.url_pack + "'>" + row.name_pack + "</a>)";
+            } else {
+                astr += "<br />(<a href='" + row.url_pack + "'>" + row.url_pack + "</a>)";
+            }
+        } else {
+            if(row.name_pack) {
+                astr += "<br />(" + row.name_pack + ")";
+            }
+        }
+        return astr;
+    },
+
+    parseSabun: function(data,type,row){
+        var str_ = "";
+
+        if(row.url_diff) {
+            if(row.name_diff) {
+                str_ += "<a href='" + row.url_diff + "'>" + row.name_diff + "</a>";
+            } else {
+                str_ += "<a href='" + row.url_diff + "'>" + SABUN_DL_STR + "</a>";
+            }
+        } else {
+            if(row.name_diff) {
+                str_ += row.name_diff;
+            }
+        }
+        return str_;
+    },
+
+    parseDate: function(data){
+        var str_ = "";
+        if(data){
+            var date_ = new Date(data);
+            str_ = date_.getFullYear() + "/" + (date_.getMonth()+1) + "/" + date_.getDate();
+        }
+        return str_;
+    },
+
+    parseTotalnotes: function(data,type,row){
+        var str_ = "";
+        if(row.total && row.notes){
+            str_ += row.total + " / " + row.notes + "<br>" + (Math.round((row.total/row.notes)*100)/100);
+        }
+        return str_;
+    },
+
+    parseType: function(data,type,row){
+        var str_ = "";
+        if(row.score_type){
+            var link = 'http://www.ribbit.xyz/bms/score/view?md5=';
+            link += row.md5;
+            str_ = '<a href="' + link + '" target="_blank">' + data + '</a>';
+        }
+        return str_;
+    },
+};
