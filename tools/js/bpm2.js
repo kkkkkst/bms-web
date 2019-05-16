b2fbaabe99b0d9716906072f5d9ce64b48e7046c
kkkkkst <mennlmkso2sr@gmail.com>
2019-05-17 02:14:08 +0900

    Prettier

--- tools/js/bpm2.js
+++ tools/js/bpm2.js
@@ -1,290 +1,286 @@
-$(function () {
-    $("#b2s").click(function () {
-        var sec = 240 / $("#bpm").val();
-        $("#mps").val($("#mpb").val() / sec);
-        app_bar();
-    });
+/* eslint-disable camelcase */
+$(function() {
+  $('#b2s').click(function() {
+    var sec = 240 / $('#bpm').val();
+    $('#mps').val($('#mpb').val() / sec);
+    app_bar();
+  });
 
+  $('#s2b').click(function() {
+    var sec = 240 / $('#bpm').val();
+    $('#mpb').val($('#mps').val() * sec);
+    app_bar();
+  });
 
-    $("#s2b").click(function () {
-        var sec = 240 / $("#bpm").val();
-        $("#mpb").val($("#mps").val() * sec);
-        app_bar();
-    });
+  $('#convert').click(function() {
+    var stra = $('#clip')
+      .val()
+      .split(/\r\n|\r|\n/);
+    var tate_haba = Number($('#tate_haba').val());
+    var tate_count = Number($('#tate_count').val());
+    var tate_ganbaru = $('#tate_ganbaru').prop('checked');
+    var tate_yuusen = $('#tate_yuusen').prop('checked');
 
+    var _str = 'iBMSC Clipboard Data xNT\n';
 
-    $("#convert").click(function () {
-        var stra = $("#clip").val().split(/\r\n|\r|\n/);
-        var tate_haba = Number($("#tate_haba").val());
-        var tate_count = Number($("#tate_count").val());
-        var tate_ganbaru = $("#tate_ganbaru").prop('checked');
-        var tate_yuusen = $("#tate_yuusen").prop('checked');
+    var lines = [];
 
-        var _str = "iBMSC Clipboard Data xNT\n";
+    var available = [5, 6, 7, 8, 9, 10, 11]; // åˆæœŸåŒ–å¿˜ã‚Œãšã«
 
-        var lines = [];
+    var note = new Note(stra[1]); // åˆæœŸåŒ–å¿˜ã‚Œãšã«
 
-        var available = [5, 6, 7, 8, 9, 10, 11]; //‰Šú‰»–Y‚ê‚¸‚É
+    var old_pos = note.getPos(); // åˆæœŸåŒ–å¿˜ã‚Œãšã«
 
-        var note = new Note(stra[1]); //‰Šú‰»–Y‚ê‚¸‚É
+    var i = 1;
 
-        var old_pos = note.getPos(); //‰Šú‰»–Y‚ê‚¸‚É
+    var used_ary = []; // åˆæœŸåŒ–å¿˜ã‚Œãšã«
 
-        var i = 1;
+    var available_pri = []; // åˆæœŸåŒ–å¿˜ã‚Œãšã«
 
-        var used_ary = []; //‰Šú‰»–Y‚ê‚¸‚É
+    var over = 26; // åˆæœŸåŒ–å¿˜ã‚Œãšã«
 
-        var available_pri = []; //‰Šú‰»–Y‚ê‚¸‚É
+    var koeta = false;
+    var turami = 0;
 
-        var over = 26; //‰Šú‰»–Y‚ê‚¸‚É
+    while (i < stra.length) {
+      // åˆæœŸåŒ–
+      available = [5, 6, 7, 8, 9, 10, 11];
+      old_pos = note.getPos();
+      used_ary = [];
+      over = 26;
 
-        var koeta = false;
-        var turami = 0;
+      // äºŒåº¦ã¨ä½¿ã‚ã‚Œãªã„éå»ã‚’å‰Šé™¤
+      while (
+        lines.length > 0 &&
+        lines[0].getPos() < note.getPos() - tate_haba * tate_count
+      ) {
+        lines.shift();
+      }
 
-        while (i < stra.length) {
+      // ç¸¦é€£ã¨ãªã‚‹å ´æ‰€ã‚’æ¤œç´¢
+      for (var j = 0; j < lines.length; j++) {
+        if (lines[j].getPos() > note.getPos() - tate_haba) {
+          Array.prototype.push.apply(used_ary, lines[j].getUse());
+        }
+      }
 
-            // ‰Šú‰»
-            available = [5, 6, 7, 8, 9, 10, 11];
-            old_pos = note.getPos();
-            used_ary = [];
-            over = 26;
-
-            // “ñ“x‚Æg‚í‚ê‚È‚¢‰ß‹‚ğíœ
-            while (lines.length > 0 && lines[0].getPos() < (note.getPos() - (tate_haba * tate_count))) {
-                lines.shift();
+      // ç¸¦é€£æ•°ãŒè¦å®šã‚’è¶…ãˆã‚‹ã‚‚ã®ã‚’æ¤œç´¢(0ãªã‚‰ã‚„ã‚“ãªã„)
+      if (tate_count != 0) {
+        var fg = 0;
+        var fg2 = 0;
+        var arr = [5, 6, 7, 8, 9, 10, 11];
+        for (var k = 1; k <= tate_count; k++) {
+          var l = note.getPos() - tate_haba * k;
+          for (var j = 0; j < lines.length; j++) {
+            if (lines[j].getPos() == l) {
+              fg = 1;
+              arr = diffArrayOverlap(arr, lines[j].getUse());
+              if (arr.length == 0) {
+                fg = 0;
+              }
+              break;
             }
+          }
 
-            // c˜A‚Æ‚È‚éêŠ‚ğŒŸõ
-            for (var j = 0; j < lines.length; j++) {
-                if (lines[j].getPos() > (note.getPos() - tate_haba)) {
-                    Array.prototype.push.apply(used_ary, lines[j].getUse());
-                }
-            }
+          if (fg == 0) {
+            fg2 = 1;
+            break;
+          }
 
-            // c˜A”‚ª‹K’è‚ğ’´‚¦‚é‚à‚Ì‚ğŒŸõ(0‚È‚ç‚â‚ñ‚È‚¢)
-            if (tate_count != 0) {
-                var fg = 0;
-                var fg2 = 0;
-                var arr = [5,6,7,8,9,10,11];
-                for (var k = 1; k <= tate_count; k++) {
-                    var l = note.getPos() - (tate_haba * k);
-                    for (var j = 0; j < lines.length; j++) {
-                        if (lines[j].getPos() == l) {
-                            fg = 1;
-                            arr = diffArrayOverlap(arr, lines[j].getUse());
-                            if (arr.length == 0) {
-                                fg = 0;
-                            }
-                            break;
-                        }
-                    }
+          fg = 0;
+        }
+        if (fg2 == 0) {
+          Array.prototype.push.apply(used_ary, arr);
+        }
+      }
 
-                    if (fg == 0) { fg2 = 1; break; }
+      // åˆ©ç”¨å¯èƒ½ã‚’å®šç¾©
+      available = diffArray(available, used_ary);
+      used_ary = [];
+      available_pri = [];
+      if (tate_yuusen) {
+        for (var j = 0; j < lines.length; j++) {
+          var l = note.getPos() - tate_haba;
+          if (lines[j].getPos() == l) {
+            available_pri = diffArray(lines[j].getUse(), available);
+            available = diffArray(available, available_pri);
+            break;
+          }
+        }
+      }
 
-                    fg = 0;
-                }
-                if (fg2 == 0) {
-                    Array.prototype.push.apply(used_ary, arr);
-                }
-            }
+      // åŒã˜Lineã‚’ãƒ«ãƒ¼ãƒ—
+      while (note.getPos() == old_pos) {
+        // åˆ©ç”¨å¯èƒ½ã«é…ç½® é…ç½®å ´æ‰€ã‚’è¨˜éŒ² ãªã‘ã‚Œã°26+ å‰Šé™¤ã‚‚
+        var ret; // é…ç½®å ´æ‰€
+        // é…ç½®å ´æ‰€æ±ºå®š
+        if (available.length != 0) {
+          // å„ªå…ˆã‚ã‚Œã°
+          if (available_pri.length != 0) {
+            var n = available_pri.length;
+            var rand = Math.floor(Math.random() * n);
+            ret = available_pri[rand];
+            available_pri.splice(rand, 1);
 
-            // —˜—p‰Â”\‚ğ’è‹`
-            available = diffArray(available, used_ary);
-            used_ary = [];
-            available_pri = [];
-            if (tate_yuusen) {
-                for (var j = 0; j < lines.length; j++) {
-                    var l = note.getPos() - tate_haba;
-                    if (lines[j].getPos() == l) {
-                        available_pri = diffArray(lines[j].getUse(), available);
-                        available = diffArray(available, available_pri);
-                        break;
-                    }
-                }
-            }
+            // ãªã‘ã‚Œã°
+          } else {
+            var n = available.length;
+            var rand = Math.floor(Math.random() * n);
+            ret = available[rand];
+            available.splice(rand, 1);
+          }
 
-            // “¯‚¶Line‚ğƒ‹[ƒv
-            while (note.getPos() == old_pos) {
+          used_ary.push(ret);
 
-                // —˜—p‰Â”\‚É”z’u ”z’uêŠ‚ğ‹L˜^ ‚È‚¯‚ê‚Î26+ íœ‚à
-                var ret; //”z’uêŠ
-                // ”z’uêŠŒˆ’è
-                if (available.length != 0) {
-
-                    //—Dæ‚ ‚ê‚Î
-                    if (available_pri.length != 0) {
-                        var n = available_pri.length;
-                        var rand = Math.floor(Math.random() * n);
-                        ret = available_pri[rand];
-                        available_pri.splice(rand, 1);
-
-                    //‚È‚¯‚ê‚Î
-                    } else {
-                        var n = available.length;
-                        var rand = Math.floor(Math.random() * n);
-                        ret = available[rand];
-                        available.splice(rand, 1);
-                    }
-
-                    used_ary.push(ret);
-
-                    //’´‚¦‚½
-                } else {
-                    if (tate_ganbaru) {
-                        koeta = true;
-                        turami++;
-                        //if (turami % 100 == 0) {
-                        //    $("#turami").text(turami);
-                        //}
-                    }
-                    ret = over;
-                    over++;
-                }
-
-                note.setCh(ret);
-
-                _str += note.getStr() + "\n";
-
-                i++;
-                if (i >= stra.length) {
-                    break;
-                }
-                note = new Note(stra[i]);
-            }
-
-            lines.push(new Line(old_pos, used_ary));
-
-            if (koeta) {
-                _str = "iBMSC Clipboard Data xNT\n";
-                lines = [];
-                note = new Note(stra[1]); //‰Šú‰»–Y‚ê‚¸‚É
-                old_pos = note.getPos(); //‰Šú‰»–Y‚ê‚¸‚É
-                i = 1;
-                koeta = false;
-            }
-
-            if (turami > 100000) {
-                _str = "tintin";
-                break;
-            }
-
+          // è¶…ãˆãŸ
+        } else {
+          if (tate_ganbaru) {
+            koeta = true;
+            turami++;
+            // if (turami % 100 == 0) {
+            //    $("#turami").text(turami);
+            // }
+          }
+          ret = over;
+          over++;
         }
 
-        $("#turami").text(turami);
-        $("#p_out").val(_str);
-    });
+        note.setCh(ret);
 
+        _str += note.getStr() + '\n';
 
-    $("#mitudo_btn").click(function () {
-        make_dens_table(parse_clip($("#mitudo_clip").val()));
-    });
-
-
-    $('#mpb').change(function () {
-        app_bar();
-    });
-
-
-    $("#left_convert").click(function () {
-        var stra = $("#left_clip").val().split(/\r\n|\r|\n/);
-        var _str = "iBMSC Clipboard Data xNT\n";
-        var left_n = $("#left_n").val();
-
-        var note = new Note(stra[1]);
-
-        var old_pos = note.getPos();
-
-        var ch = 26;
-
-        var ch_ = 57;
-
-
-
-
-        for (var i = 1; i < stra.length; i++) {
-
-            note = new Note(stra[i]);
-
-            if (note.getPos() != old_pos) {
-                ch = 26;
-                ch_ = 57;
-            }
-
-            if (note.getPos() % left_n == 0) {
-                note.setCh(ch)
-                ch++;
-            } else {
-                note.setCh(ch_)
-                ch_--;
-            }
-
-            old_pos = note.getPos();
-
-            _str += note.getStr() + "\n";
+        i++;
+        if (i >= stra.length) {
+          break;
         }
+        note = new Note(stra[i]);
+      }
 
-        $("#left_out").val(_str);
-    });
+      lines.push(new Line(old_pos, used_ary));
 
+      if (koeta) {
+        _str = 'iBMSC Clipboard Data xNT\n';
+        lines = [];
+        note = new Note(stra[1]); // åˆæœŸåŒ–å¿˜ã‚Œãšã«
+        old_pos = note.getPos(); // åˆæœŸåŒ–å¿˜ã‚Œãšã«
+        i = 1;
+        koeta = false;
+      }
 
-    $("#bt6_convert").click(function () {
-        var stra = $("#bt6_clip").val().split(/\r\n|\r|\n/);
-        var bpm = Number($("#bt6_bpm").val());
-        var speed = Number($("#bt6_speed").val());
-        var start = Number($("#bt6_start").val());
-        var delay = Number($("#bt6_delay").val());
-        var start_time = start * 192;
-        var h_time = (60 / bpm) / 48;
-        var out_str = "";
+      if (turami > 100000) {
+        _str = 'tintin';
+        break;
+      }
+    }
 
-        for (var i = 1; i < stra.length; i++) {
-            var note = new Note(stra[i]);
-            var out_arr = new Array(4);
+    $('#turami').text(turami);
+    $('#p_out').val(_str);
+  });
 
-            var sec = h_time * (Number(note.getPos()) + start_time + delay);
-            //sec = Math.round(sec*100);
+  $('#mitudo_btn').click(function() {
+    make_dens_table(parse_clip($('#mitudo_clip').val()));
+  });
 
-            out_arr[0] = sec;// / 100;
+  $('#mpb').change(function() {
+    app_bar();
+  });
 
-            if(note.getObj() == "10000"){
-                out_arr[1] = "enemy";
-            }else{
-                out_arr[1] = "friend";
-            }
+  $('#left_convert').click(function() {
+    var stra = $('#left_clip')
+      .val()
+      .split(/\r\n|\r|\n/);
+    var _str = 'iBMSC Clipboard Data xNT\n';
+    var left_n = $('#left_n').val();
 
-            if(note.getCh() == "5"){
-                out_arr[2] = "L";
-            }else if(note.getCh() == "7"){
-                out_arr[2] = "D";
-            }else if(note.getCh() == "9"){
-                out_arr[2] = "U";
-            }else{
-                out_arr[2] = "R";
-            }
+    var note = new Note(stra[1]);
 
-            out_arr[3] = speed;
+    var old_pos = note.getPos();
 
-            console.log(out_arr);
+    var ch = 26;
 
-            out_str += "\n\t\t[" + out_arr[0] + ", \"" + out_arr[1] + "\", \"" + out_arr[2] + "\", " + out_arr[3] + "],";
-        }
+    var ch_ = 57;
 
+    for (var i = 1; i < stra.length; i++) {
+      note = new Note(stra[i]);
 
-        $("#bt6_out").val(out_str);
+      if (note.getPos() != old_pos) {
+        ch = 26;
+        ch_ = 57;
+      }
 
+      if (note.getPos() % left_n == 0) {
+        note.setCh(ch);
+        ch++;
+      } else {
+        note.setCh(ch_);
+        ch_--;
+      }
 
-    });
+      old_pos = note.getPos();
 
+      _str += note.getStr() + '\n';
+    }
 
-    var clipboard = new Clipboard('#p_copy');
-    var clipboard = new Clipboard('#left_copy');
-    var clipboard = new Clipboard('#bt6_cpy');
+    $('#left_out').val(_str);
+  });
 
+  $('#bt6_convert').click(function() {
+    var stra = $('#bt6_clip')
+      .val()
+      .split(/\r\n|\r|\n/);
+    var bpm = Number($('#bt6_bpm').val());
+    var speed = Number($('#bt6_speed').val());
+    var start = Number($('#bt6_start').val());
+    var delay = Number($('#bt6_delay').val());
+    var start_time = start * 192;
+    var h_time = 60 / bpm / 48;
+    var out_str = '';
 
+    for (var i = 1; i < stra.length; i++) {
+      var note = new Note(stra[i]);
+      var out_arr = new Array(4);
 
+      var sec = h_time * (Number(note.getPos()) + start_time + delay);
+      // sec = Math.round(sec*100);
 
+      out_arr[0] = sec; // / 100;
 
+      if (note.getObj() == '10000') {
+        out_arr[1] = 'enemy';
+      } else {
+        out_arr[1] = 'friend';
+      }
 
+      if (note.getCh() == '5') {
+        out_arr[2] = 'L';
+      } else if (note.getCh() == '7') {
+        out_arr[2] = 'D';
+      } else if (note.getCh() == '9') {
+        out_arr[2] = 'U';
+      } else {
+        out_arr[2] = 'R';
+      }
 
+      out_arr[3] = speed;
 
+      console.log(out_arr);
+
+      out_str +=
+        '\n\t\t[' +
+        out_arr[0] +
+        ', "' +
+        out_arr[1] +
+        '", "' +
+        out_arr[2] +
+        '", ' +
+        out_arr[3] +
+        '],';
+    }
+
+    $('#bt6_out').val(out_str);
+  });
+
+  var clipboard = new Clipboard('#p_copy');
+  var clipboard = new Clipboard('#left_copy');
+  var clipboard = new Clipboard('#bt6_cpy');
 });
