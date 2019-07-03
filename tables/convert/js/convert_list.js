b691bd3a2e137fa56c88e719dd6a92a4e2410974
kkkkkst <mennlmkso2sr@gmail.com>
2019-07-04 06:09:18 +0900

    hugo

--- tables/convert/js/convert_list.js
+++ tables/convert/js/convert_list.js
@@ -1,61 +1,61 @@
 $(document).ready(function() {
-  $('#convertTableList').DataTable({
+  $("#convertTableList").DataTable({
     lengthChange: false,
     info: false,
     paging: false,
-    order: [1, 'asc'],
+    order: [1, "asc"],
 
     language: {
-      url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Japanese.json',
+      url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/Japanese.json"
     },
 
     ajax: {
-      url: 'list.json',
-      dataSrc: '',
+      url: "list.json",
+      dataSrc: ""
     },
 
     columns: [
       {
-        title: '★',
-        data: 'symbol',
+        title: "★",
+        data: "symbol"
       },
       {
-        title: '差分サイト<br>(変換後ページ)',
-        data: 'name',
+        title: "差分サイト<br>(変換後ページ)",
+        data: "name",
         render: function(data, type, row) {
-          return '<a href="' + row.url + '" target="_blank">' + data + '</a>';
-        },
+          return '<a href="' + row.url + '" target="_blank">' + data + "</a>";
+        }
       },
       {
-        title: '変換元',
-        data: 'original_url',
+        title: "変換元",
+        data: "original_url",
         render: function(data, type, row) {
-          return '<a href="' + data + '" target="_blank">' + data + '</a>';
-        },
+          return '<a href="' + data + '" target="_blank">変換元</a>';
+        }
       },
       {
-        title: 'Last Update',
-        data: 'update',
-      },
-    ],
+        title: "Last Update",
+        data: "update"
+      }
+    ]
   });
 
-  new ClipboardJS('#cotCopy');
+  new ClipboardJS("#cotCopy");
 
-  $('#cotExec').click(function() {
+  $("#cotExec").click(function() {
     var url =
-      'https://script.google.com/macros/s/AKfycbxMG5jqTHVZqSqE3dNkvc-QUcgm1-soiy0xMOJQn0ycCy2ZBGiy/exec?convoldtable=' +
-      encodeURI($('#cotName').val()) +
-      '&url=' +
-      encodeURI($('#cotUrl').val()) +
-      '&symbol=' +
-      encodeURI($('#cotSymbol').val()) +
-      '&encode=' +
-      encodeURI($('#cotEncode').val());
-    $('#cotOutput').val(url);
+      "https://script.google.com/macros/s/AKfycbxMG5jqTHVZqSqE3dNkvc-QUcgm1-soiy0xMOJQn0ycCy2ZBGiy/exec?convoldtable=" +
+      encodeURI($("#cotName").val()) +
+      "&url=" +
+      encodeURI($("#cotUrl").val()) +
+      "&symbol=" +
+      encodeURI($("#cotSymbol").val()) +
+      "&encode=" +
+      encodeURI($("#cotEncode").val());
+    $("#cotOutput").val(url);
   });
 
-  $('.cotAutoSelect').focus(function() {
+  $(".cotAutoSelect").focus(function() {
     $(this).select();
   });
 });
