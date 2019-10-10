d86cf9ce8e4eaef736ecf34b531bd61d4bd26b4f
kkkkkst <mennlmkso2sr@gmail.com>
2019-10-11 04:11:50 +0900

    hugo

--- others/js/sabunlist.js
+++ others/js/sabunlist.js
@@ -1,10 +1,24 @@
+jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+  "non-empty-string-asc": function(str1, str2) {
+    if (str1 == "") return 1;
+    if (str2 == "") return -1;
+    return str1 < str2 ? -1 : str1 > str2 ? 1 : 0;
+  },
+
+  "non-empty-string-desc": function(str1, str2) {
+    if (str1 == "") return 1;
+    if (str2 == "") return -1;
+    return str1 < str2 ? 1 : str1 > str2 ? -1 : 0;
+  }
+});
+
 $(document).ready(function() {
   $("#tableList").DataTable({
     lengthChange: true,
     info: false,
     paging: false,
     scrollX: false,
-    order: [[0, "asc"], [2, "asc"]],
+    order: [[0, "asc"], [3, "asc"], [1, "asc"]],
 
     language: {
       url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/Japanese.json"
@@ -19,13 +33,11 @@ $(document).ready(function() {
     columns: [
       {
         title: "タイプ",
-        data: "type",
-        orderData: [1]
-      },
-      {
-        title: "ot",
-        data: "type_order",
-        visible: false
+        data: {
+          _: "type",
+          sort: "type_order",
+          display: "type"
+        }
       },
       {
         title: "差分公開サイト",
@@ -36,28 +48,48 @@ $(document).ready(function() {
       },
       {
         title: "管理人",
+        type: "non-empty-string",
         data: "administrator"
       },
       {
         title: "難易度表 (json)",
-        data: "name_table",
-        render: function(data, type, row) {
-          return (
-            '<a href="' + row.url_table + '" target="_blank">' + data + "</a>"
-          );
+        type: "non-empty-string",
+        data: function(row, type, set, meta) {
+          if (type === "display") {
+            return (
+              '<a href="' +
+              row.url_table +
+              '" target="_blank">' +
+              row.name_table +
+              "</a>"
+            );
+          } else if (type === "sort") {
+            return row.name_table;
+          }
+          return row.name_table;
         }
       },
       {
         title: "ミラー",
-        data: "name_mirror",
-        render: function(data, type, row) {
-          return (
-            '<a href="' + row.url_mirror + '" target="_blank">' + data + "</a>"
-          );
+        type: "non-empty-string",
+        data: function(row, type, set, meta) {
+          if (type === "display") {
+            return (
+              '<a href="' +
+              row.url_mirror +
+              '" target="_blank">' +
+              row.name_mirror +
+              "</a>"
+            );
+          } else if (type === "sort") {
+            return row.name_mirror;
+          }
+          return row.name_mirror;
         }
       },
       {
         title: "備考",
+        type: "non-empty-string",
         data: "note"
       }
     ]
